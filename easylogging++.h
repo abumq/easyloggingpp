80c96a3c2c6c0abebe5ca9273db0ffbebc827d75
Majid Khan <mkhan@allhomes.com.au>
2012-10-23 16:58:26 +1100

    v2.1 fixes time issue

--- easylogging++.h
+++ easylogging++.h
@@ -1,6 +1,6 @@
-/************************************************************************\
+/***********************************************************************\
 * easylogging++.h - Core of EasyLogging++                              *
-*   EasyLogging++ v2.0                                                 *
+*   EasyLogging++ v2.1                                                 *
 *   Cross platform logging made easy for C++ applications              *
 *   Author Majid Khan <mkhan3189@gmail.com>                            *
 *   http://www.icplusplus.com                                          *
@@ -20,7 +20,7 @@
 #define _LOGGING_ENABLED 1
 
 #define _ENABLE_DEBUG_LOGS 1
-#define _DEBUG_LOGS_TO_STANDARD_OUTPUT 1
+#define _DEBUG_LOGS_TO_STANDARD_OUTPUT 0
 #define _DEBUG_LOGS_TO_FILE 1
 
 #define _ENABLE_INFO_LOGS 1
@@ -40,13 +40,14 @@
 #define _FATAL_LOGS_TO_FILE 1
 
 #define _ENABLE_PERFORMANCE_LOGS 1
-#define _PERFORMANCE_LOGS_TO_STANDARD_OUTPUT 1
+#define _PERFORMANCE_LOGS_TO_STANDARD_OUTPUT 0
 #define _PERFORMANCE_LOGS_TO_FILE 1
 
 #if _LOGGING_ENABLED
 #include <iostream>
 #include <sstream>
 #include <fstream>
+#include <ctime>
 //////////////////////////////////////////////
 //     Configuration for logging           ///
 //////////////////////////////////////////////
@@ -74,12 +75,12 @@ const bool SHOW_TIME = true;
 /**
 * Flag to set whether to show which file logged the output and what line
 */
-const bool SHOW_LOG_LOCATION = true;
+const bool SHOW_LOG_LOCATION = false;
 
 /**
 * Flag to set whether to show which function logged the output
 */
-const bool SHOW_LOG_FUNCTION = true;
+const bool SHOW_LOG_FUNCTION = false;
 
 /**
 * Flag to set whether output value of NOT_SUPPORTED_STRING if extra info is not available on machine
@@ -128,12 +129,20 @@ const bool EXTRA_INFO_ENABLED = SHOW_DATE || SHOW_TIME || SHOW_LOG_LOCATION || S
 static std::stringstream *streamForEasyLoggingPP;
 static bool ELPPtoStdOut;
 static bool ELPPtoFile;
-#ifndef __DATE__
- #define __DATE__ (SHOW_NOT_SUPPORTED_ON_NO_EXTRA_INFO) ? NOT_SUPPORTED_STRING : ""
-#endif
-#ifndef __TIME__
- #define __TIME__ (SHOW_NOT_SUPPORTED_ON_NO_EXTRA_INFO) ? NOT_SUPPORTED_STRING : ""
-#endif
+static char ELPPdateBuff[21];
+inline static std::string getELPPDateTime(void) {
+  if (!(SHOW_DATE || SHOW_TIME)) return "";
+  time_t rawtime;
+  struct tm * timeinfo;
+  time (&rawtime);
+  timeinfo = localtime (&rawtime);
+  std::string format = "";
+  if (SHOW_DATE) format += "%d/%m/%Y";
+  if (SHOW_TIME) format += (std::string((SHOW_DATE ? " " : "")) + std::string("%H:%M:%S"));
+  strftime (ELPPdateBuff,21,format.c_str(),timeinfo);
+  std::string buffStr(ELPPdateBuff);
+  return buffStr;
+}
 #ifndef __FILE__
  #define __FILE__ (SHOW_NOT_SUPPORTED_ON_NO_EXTRA_INFO) ? NOT_SUPPORTED_STRING : ""
 #endif
@@ -147,7 +156,7 @@ static bool ELPPtoFile;
 #else
  #define __func__ (SHOW_NOT_SUPPORTED_ON_NO_EXTRA_INFO) ? NOT_SUPPORTED_STRING : ""
 #endif
-inline static void writeLogNow() {
+static void writeLogNow() {
     if (SHOW_STD_OUTPUT && ELPPtoStdOut) {
         std::cout << streamForEasyLoggingPP->str() << std::endl;
     }
@@ -171,19 +180,9 @@ inline static void writeLogNow() {
     { streamForEasyLoggingPP = new std::stringstream(); } \
     (*streamForEasyLoggingPP) << "[" << type << "]";\
     if (SHOW_DATE || SHOW_TIME) {\
-      (*streamForEasyLoggingPP) << " [";\
-      if (SHOW_DATE) {\
-        (*streamForEasyLoggingPP) << __DATE__;\
-      }\
-     if (SHOW_TIME) {\
-       if (SHOW_DATE) {\
-         (*streamForEasyLoggingPP) << " ";\
-       }\
-       (*streamForEasyLoggingPP) << __TIME__;\
-     }\
-     (*streamForEasyLoggingPP) << "]";\
+      (*streamForEasyLoggingPP) << " [" << getELPPDateTime() << "]";\
     }\
-	if (SHOW_LOG_FUNCTION) {\
+    if (SHOW_LOG_FUNCTION) {\
       (*streamForEasyLoggingPP) << " [" << __func__ << "]";\
     }\
     if (SHOW_LOG_LOCATION) {\
@@ -224,10 +223,21 @@ inline static void writeLogNow() {
     #define FATAL(x)
   #endif//_ENABLE_FATAL_LOGS
   #if _ENABLE_PERFORMANCE_LOGS
-    #include <ctime>
+    inline static std::string formatELPPSeconds(double secs) {
+        double result = secs;
+        std::string unit = "seconds";
+        std::stringstream ss;
+        if (result > 60.0f) {result /= 60; unit = "minutes";
+            if (result > 60.0f) {result /= 60; unit = "hours";
+                if (result > 24.0f) {result /= 24; unit = "days";}
+            }
+        }
+        ss << result << " " << unit;
+        return ss.str();
+    }
     #define PERFORMANCE(logStr) LOG("PERFORMANCE",logStr)
     #define START_FUNCTION_LOG "Executing [" << __func__ << "]"
-    #define TIME_OUTPUT "Executed [" << __func__ << "] in [~" << difftime (functionEndTime,functionStartTime) << " seconds]"
+    #define TIME_OUTPUT "Executed [" << __func__ << "] in [~ " << formatELPPSeconds(difftime(functionEndTime,functionStartTime)) << "]"
     #define FUNC_SUB_COMMON_START { if (SHOW_START_FUNCTION_LOG) { PERFORMANCE(START_FUNCTION_LOG) } time_t functionStartTime,functionEndTime; time(&functionStartTime);
     #define FUNC_SUB_COMMON_END time(&functionEndTime); PERFORMANCE(TIME_OUTPUT);
     #define SUB(FUNCTION_NAME,PARAMS) void FUNCTION_NAME PARAMS FUNC_SUB_COMMON_START 
