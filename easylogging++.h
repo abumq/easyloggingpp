0073733e5b319a192c7b5fa0f7e2e4068553dc85
mkhan <mkhan@allhomes.com.au>
2012-11-13 14:23:00 +1100

    v2.3 added host and username included in logs

--- easylogging++.h
+++ easylogging++.h
@@ -1,6 +1,6 @@
 /***********************************************************************\
 * easylogging++.h - Core of EasyLogging++                              *
-*   EasyLogging++ v2.2                                                 *
+*   EasyLogging++ v2.3                                                 *
 *   Cross platform logging made easy for C++ applications              *
 *   Author Majid Khan <mkhan3189@gmail.com>                            *
 *   http://www.icplusplus.com                                          *
@@ -27,15 +27,15 @@
 #define _INFO_LOGS_TO_STANDARD_OUTPUT 1
 #define _INFO_LOGS_TO_FILE 1
 
-#define _ENABLE_WARNING_LOGS 1 
+#define _ENABLE_WARNING_LOGS 1
 #define _WARNING_LOGS_TO_STANDARD_OUTPUT 1
 #define _WARNING_LOGS_TO_FILE 1
 
-#define _ENABLE_ERROR_LOGS 1 
+#define _ENABLE_ERROR_LOGS 1
 #define _ERROR_LOGS_TO_STANDARD_OUTPUT 1
 #define _ERROR_LOGS_TO_FILE 1
 
-#define _ENABLE_FATAL_LOGS 1 
+#define _ENABLE_FATAL_LOGS 1
 #define _FATAL_LOGS_TO_STANDARD_OUTPUT 1
 #define _FATAL_LOGS_TO_FILE 1
 
@@ -56,6 +56,8 @@
 #include <sstream>
 #include <fstream>
 #include <ctime>
+#include <cstring>
+#include <cstdlib>
 //////////////////////////////////////////////
 //     Configuration for logging           ///
 //////////////////////////////////////////////
@@ -89,6 +91,16 @@ const bool SHOW_LOG_LOCATION = false;
 * Flag to set whether to show which function logged the output
 */
 const bool SHOW_LOG_FUNCTION = false;
+
+/**
+* Flag to set whether to show username or not
+*/
+const bool SHOW_USERNAME = true;
+
+/**
+* Flag to set whether to show hostname or not
+*/
+const bool SHOW_HOSTNAME = true;
 
 /**
 * Flag to set whether output value of NOT_SUPPORTED_STRING if extra info is not available on machine
@@ -133,24 +145,46 @@ const bool SHOW_START_FUNCTION_LOG = false;
 ///               END OF CONFIGURATION FOR LOGGING               ///
 ////////////////////////////////////////////////////////////////////
 
-const bool EXTRA_INFO_ENABLED = SHOW_DATE || SHOW_TIME || SHOW_LOG_LOCATION || SHOW_LOG_FUNCTION;
+const bool EXTRA_INFO_ENABLED = SHOW_DATE || SHOW_TIME || SHOW_LOG_LOCATION || SHOW_LOG_FUNCTION || SHOW_USERNAME || SHOW_HOSTNAME;
 static std::stringstream *streamForEasyLoggingPP;
 static bool ELPPtoStdOut;
 static bool ELPPtoFile;
-static char ELPPdateBuff[21];
+static char ELPPdateBuff[25];
+namespace easyloggingpp {
 inline static std::string getELPPDateTime(void) {
   if (!(SHOW_DATE || SHOW_TIME)) return "";
-  time_t rawtime;
-  struct tm * timeinfo;
-  time (&rawtime);
-  timeinfo = localtime (&rawtime);
-  std::string format = "";
-  if (SHOW_DATE) format += "%d/%m/%Y";
-  if (SHOW_TIME) format += (std::string((SHOW_DATE ? " " : "")) + std::string("%H:%M:%S"));
-  strftime (ELPPdateBuff,21,format.c_str(),timeinfo);
+#if _WIN32
+  char* envDate = getenv("DATE");
+  char* envTime = getenv("TIME");
+  if ((envDate == NULL) || (envTime == NULL) || ((strcmp(envDate, "")) || (strcmp(envTime, "")))) {
+#endif
+    time_t rawtime;
+    struct tm * timeinfo;
+    time (&rawtime);
+    timeinfo = localtime (&rawtime);
+    std::string format = "";
+    if (SHOW_DATE) format += "%d/%m/%Y";
+    if (SHOW_TIME) format += (std::string((SHOW_DATE ? " " : "")) + std::string("%H:%M:%S"));
+    strftime (ELPPdateBuff,21,format.c_str(),timeinfo);
+#if _WIN32
+  } else {
+    if (SHOW_DATE) {
+      strcpy(ELPPdateBuff, envDate);
+    }
+    if (SHOW_TIME) {
+      if (SHOW_DATE) {
+        strcat(ELPPdateBuff, " ");
+        strcat(ELPPdateBuff, envTime);
+      } else {
+        strcpy(ELPPdateBuff, envTime);
+      }
+   }
+  }
+#endif
   std::string buffStr(ELPPdateBuff);
   return buffStr;
 }
+} //namespace easyloggingpp
 #ifndef __FILE__
  #define __FILE__ (SHOW_NOT_SUPPORTED_ON_NO_EXTRA_INFO) ? NOT_SUPPORTED_STRING : ""
 #endif
@@ -164,6 +198,22 @@ inline static std::string getELPPDateTime(void) {
 #else
  #define __func__ (SHOW_NOT_SUPPORTED_ON_NO_EXTRA_INFO) ? NOT_SUPPORTED_STRING : ""
 #endif
+namespace easyloggingpp {
+static inline char* getUsername(void) {
+  return getenv("USERNAME");
+}
+static inline std::string getHostname(void) {
+#if _WIN32
+  char* hostname = getenv("COMPUTERNAME");
+#else
+  char* hostname = getenv("HOSTNAME");
+#endif
+  if ((hostname == NULL) || ((strcmp(hostname, "") == 0))) {
+    return std::string("Unknown Host");
+  } else {
+    return std::string(hostname);
+  }
+}
 static void writeLogNow() {
     if (SHOW_STD_OUTPUT && ELPPtoStdOut) {
         std::cout << streamForEasyLoggingPP->str() << std::endl;
@@ -184,11 +234,18 @@ static void writeLogNow() {
         streamForEasyLoggingPP = 0;
     }
 }
+} //namespace easyloggingpp
 #define WRITE_LOG(type,log) if (streamForEasyLoggingPP == 0) \
     { streamForEasyLoggingPP = new std::stringstream(); } \
     (*streamForEasyLoggingPP) << "[" << type << "]";\
     if (SHOW_DATE || SHOW_TIME) {\
-      (*streamForEasyLoggingPP) << " [" << getELPPDateTime() << "]";\
+      (*streamForEasyLoggingPP) << " [" << ::easyloggingpp::getELPPDateTime() << "]";\
+    }\
+    if (SHOW_USERNAME) {\
+        (*streamForEasyLoggingPP) << " [" << ::easyloggingpp::getUsername() << (SHOW_HOSTNAME ? "" : "]") ;\
+    }\
+    if (SHOW_HOSTNAME) {\
+        (*streamForEasyLoggingPP) << (SHOW_USERNAME ? "@" : " [") << easyloggingpp::getHostname() << "]";\
     }\
     if (SHOW_LOG_FUNCTION) {\
       (*streamForEasyLoggingPP) << " [" << __func__ << "]";\
@@ -205,7 +262,7 @@ static void writeLogNow() {
       if (type == "PERFORMANCE") {ELPPtoStdOut = _PERFORMANCE_LOGS_TO_STANDARD_OUTPUT; ELPPtoFile = _PERFORMANCE_LOGS_TO_FILE;}\
       if (type == "HINT") {ELPPtoStdOut = _HINTS_TO_STANDARD_OUTPUT; ELPPtoFile = _HINTS_TO_FILE;}\
       if (type == "STATUS") {ELPPtoStdOut = _STATUS_TO_STANDARD_OUTPUT; ELPPtoFile = _STATUS_TO_FILE;}\
-    writeLogNow();
+    ::easyloggingpp::writeLogNow();
  
   #if _ENABLE_DEBUG_LOGS
     #define DEBUG(logStr) WRITE_LOG("DEBUG",logStr)
@@ -233,6 +290,7 @@ static void writeLogNow() {
     #define FATAL(x)
   #endif//_ENABLE_FATAL_LOGS
   #if _ENABLE_PERFORMANCE_LOGS
+    namespace easyloggingpp {
     inline static std::string formatELPPSeconds(double secs) {
         double result = secs;
         std::string unit = "seconds";
@@ -245,9 +303,10 @@ static void writeLogNow() {
         ss << result << " " << unit;
         return ss.str();
     }
+    } //namespace easyloggingpp
     #define PERFORMANCE(logStr) WRITE_LOG("PERFORMANCE",logStr)
     #define START_FUNCTION_LOG "Executing [" << __func__ << "]"
-    #define TIME_OUTPUT "Executed [" << __func__ << "] in [~ " << formatELPPSeconds(difftime(functionEndTime,functionStartTime)) << "]"
+    #define TIME_OUTPUT "Executed [" << __func__ << "] in [~ " << ::easyloggingpp::formatELPPSeconds(difftime(functionEndTime,functionStartTime)) << "]"
     #define FUNC_SUB_COMMON_START { if (SHOW_START_FUNCTION_LOG) { PERFORMANCE(START_FUNCTION_LOG) } time_t functionStartTime,functionEndTime; time(&functionStartTime);
     #define FUNC_SUB_COMMON_END time(&functionEndTime); PERFORMANCE(TIME_OUTPUT);
     #define SUB(FUNCTION_NAME,PARAMS) void FUNCTION_NAME PARAMS FUNC_SUB_COMMON_START 
@@ -267,12 +326,12 @@ static void writeLogNow() {
     #define HINT(logStr) WRITE_LOG("HINT",logStr)
   #else
     #define HINT(x)
-  #endif
+  #endif //_ENABLE_HINTS
   #if _ENABLE_STATUS
     #define STATUS(logStr) WRITE_LOG("STATUS",logStr)
   #else
     #define STATUS(x)
-  #endif
+  #endif //_ENABLE_STATUS
 #else
   #define DEBUG(x)
   #define INFO(x)
