fbb704828aea7c665b3a2f534d66fe5742dac225
mkhan3189 <mkhan3189@gmail.com>
2012-11-26 21:37:11 +1100

    Added speed test ~30 logs per millisecond

---
+++ samples/speed_test.cpp
@@ -0,0 +1,21 @@
+/**
+ * This file is part of EasyLogging++ samples
+ * Demonstration of how fast can EasyLogging++ log
+ * @author mkhan3189
+ */
+#include "../easylogging++.h"
+FUNC(int, main, (void))
+  STATUS("PROGRAM STARTED");
+  DEBUG("This is start of logging!");
+  for (int i = 1; i <= 1000000; i++) {
+    INFO("This is a test number " << i);
+  }
+  STATUS("Completed!");
+  INFO("Now EasyLogging++ is logging DEBUG logs that are stored in logs/myeasylog.log by default");
+  for (int i = 1; i <= 100; i++) {
+    DEBUG("Some more logs [" << i << "]");
+  }
+  DEBUG("This is end of logging!");
+  STATUS("PROGRAM ENDED");
+END_FUNC
+
