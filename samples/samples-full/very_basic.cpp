87315487ae687a02c2e6b45e093173af5e267a59
Majid <mkhan3189@gmail.com>
2013-02-10 16:06:44 +1100

    Update samples/samples-full/very_basic.cpp

--- samples/samples-full/very_basic.cpp
+++ samples/samples-full/very_basic.cpp
@@ -7,9 +7,9 @@
 
 #include "../../easylogging++-full.h" // Include header
 
-_INITIALIZE_EASYLOGGINGPP     // Initialize
+_INITIALIZE_EASYLOGGINGPP     // Initialize - Needed only once in your application
 
 int main(int argc, char** argv) {
   INFO("This is first log!"); // Write
-  _END_EASYLOGGINGPP          // Release all memory
+  _END_EASYLOGGINGPP          // Release all memory - Needed only once in your application
 }
