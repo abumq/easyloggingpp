0146edabd9d025f54d3b51151a99943c83b79ec7
Majid Khan <mkhan3189@gmail.com>
2013-03-24 01:40:19 +1100

    container log imporvements

--- samples/container_log.cpp
+++ samples/container_log.cpp
@@ -1,3 +1,11 @@
+/**
+ * This file is part of EasyLogging++ samples
+ * Demonstration of container logging using EasyLogging++
+ *
+ * Revision 1.0
+ * @author mkhan3189
+ */
+
 #include <vector>
 #include "../easylogging++.h"
 
@@ -6,5 +14,12 @@ _INITIALIZE_EASYLOGGINGPP
 int main(void) {
     std::vector<std::string> v = { "string 1", "string 2" };
     LINFO << "Now I will print std::vector<std::string> and values are --> " << v << " <-- This is end of log";
+
+    std::vector<int> intVec_ = { 1, 2, 3, 4, 5 };
+    BINFO << "Business info log for int container " << intVec_;
+
+    std::vector<bool> boolVec_ = { true, false, true, true, true, false };
+    SWARNING << "Security warning log for bool container " << boolVec_;
+    
     return 0; 
 }
