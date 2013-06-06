32f359d80484fbbc3841c8dba072d8d5468f95c3
Majid Khan <mkhan3189@gmail.com>
2013-06-06 22:07:17 +1000

    added log configuration for ConfigurationBuilder

--- tools/ConfigurationBuilder/main.cpp
+++ tools/ConfigurationBuilder/main.cpp
@@ -4,8 +4,19 @@
 
 _INITIALIZE_EASYLOGGINGPP
 
+using namespace easyloggingpp;
+
 int main(int argc, char *argv[])
 {
+    Configurations c;
+    c.setToDefault();
+#if _ELPP_OS_UNIX
+    c.parseFromFile("../ConfigurationBuilder/log-conf/unix.log");
+#elif _ELPP_OS_WINDOWS
+    c.parseFromFile("..\\ConfigurationBuilder\\log-conf\\win.log");
+#endif // _ELPP_OS_UNIX
+    Loggers::reconfigureAllLoggers(c);
+
     QApplication a(argc, argv);
     MainDialog w;
     w.show();
