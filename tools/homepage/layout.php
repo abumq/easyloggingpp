84a3a1c1720431b4a03fbcaf64c9456f79b8bfd7
mkhan3189 <mkhan3189@gmail.com>
2013-08-16 00:04:11 +1000

    Images and logos

--- tools/homepage/layout.php
+++ tools/homepage/layout.php
@@ -1,6 +1,7 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
     <head>
+        <meta property="og:image" content="http://easylogging.org/images/logo-big.png"/>
         <title>Easylogging++ - Single header only, cross-platform logging library for C++ applications</title>
         <script type="text/javascript" src="/script/jquery.js"></script>
         <link rel="stylesheet" type="text/css" href="/style/layout.css" />
