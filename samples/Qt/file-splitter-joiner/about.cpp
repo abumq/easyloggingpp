b4522fe457f14542bf1aed22739455ac1e43cee0
mkhan <mkhan@allhomes.com.au>
2013-08-19 10:51:25 +1000

    sample update

--- samples/Qt/file-splitter-joiner/about.cpp
+++ samples/Qt/file-splitter-joiner/about.cpp
@@ -1,11 +1,13 @@
 #include "about.h"
 #include "ui_about.h"
 #include <QMessageBox>
+#include "easylogging++.h"
 
 About::About(QWidget *parent) :
     QWidget(parent),
     ui(new Ui::About) {
     ui->setupUi(this);
+    ui->el_info->setText(QString(el::VersionInfo::formattedInfo().c_str()));
 }
 
 About::~About() {
--- samples/Qt/file-splitter-joiner/about.h
+++ samples/Qt/file-splitter-joiner/about.h
@@ -3,6 +3,7 @@
 
 #include <QWidget>
 
+
 namespace Ui {
 class About;
 }
