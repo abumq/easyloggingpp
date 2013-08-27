e8f9f56db2a52b3715bafaed12cddb6b7ea28a71
mkhan <mkhan@allhomes.com.au>
2013-08-28 09:46:23 +1000

    v9.13 fixes issue with log files creation

--- src/easylogging++.h
+++ src/easylogging++.h
@@ -1,5 +1,5 @@
 //
-//  Easylogging++ v9.12 (development / unreleased version)
+//  Easylogging++ v9.13
 //  Single-header only, cross-platform logging library for C++ applications
 //
 //  Author Majid Khan
@@ -4415,10 +4415,10 @@ public:
     }
 
     /// @brief Current version number
-    static inline const std::string version(void) { return std::string("9.12"); }
+    static inline const std::string version(void) { return std::string("9.13"); }
 
     /// @brief Release date of current version
-    static inline const std::string releaseDate(void) { return std::string("27-08-2013 1935hrs"); }
+    static inline const std::string releaseDate(void) { return std::string("28-08-2013 0945hrs"); }
 
     /// @brief Original author and maintainer
     static inline const std::string author(void) { return std::string("Majid Khan"); }
