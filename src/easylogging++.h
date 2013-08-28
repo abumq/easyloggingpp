8caeedb45aa11abe6af47b248f78011280eae85c
mkhan <mkhan@allhomes.com.au>
2013-08-28 10:37:30 +1000

    v9.15 unused warnings fixes

--- src/easylogging++.h
+++ src/easylogging++.h
@@ -1,5 +1,5 @@
 //
-//  Easylogging++ v9.14 (development / unreleased version)
+//  Easylogging++ v9.15
 //  Single-header only, cross-platform logging library for C++ applications
 //
 //  Author Majid Khan
@@ -4422,10 +4422,10 @@ public:
     }
 
     /// @brief Current version number
-    static inline const std::string version(void) { return std::string("9.14"); }
+    static inline const std::string version(void) { return std::string("9.15"); }
 
     /// @brief Release date of current version
-    static inline const std::string releaseDate(void) { return std::string("28-08-2013 1015hrs"); }
+    static inline const std::string releaseDate(void) { return std::string("28-08-2013 1037hrs"); }
 
     /// @brief Original author and maintainer
     static inline const std::string author(void) { return std::string("Majid Khan"); }
