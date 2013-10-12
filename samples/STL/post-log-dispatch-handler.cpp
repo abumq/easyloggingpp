39b0c22b18fcd11f64d5541a9f32e66a8c314582
mkhan3189 <mkhan3189@gmail.com>
2013-10-13 00:19:19 +1100

    post-log-dispatch sample improved

--- samples/STL/post-log-dispatch-handler.cpp
+++ samples/STL/post-log-dispatch-handler.cpp
@@ -11,19 +11,28 @@
 
 _INITIALIZE_EASYLOGGINGPP
 
-void handler(const el::LogMessage*) {
+static std::vector<std::string> log_messages_dispatched;
+
+void handler(const el::LogMessage* msg) {
     // NOTE:
     // SHOULD NOT LOG ANYTHING HERE! WE DON'T LIKE RECURSIVE LOG-DISPATCHES!
 
     // BUT WE CAN USE std::cout :)
-    std::cout << "Log dispatch has been handled";
+    log_messages_dispatched.push_back(msg->message());
+
+    // Also note, this msg pointer will become unavailable as soon as this handler is done.
+    // so there will be no point of having a vector like std::vector<el::LogMessage*> and store these pointers
 }
 
 int main(void) {
    
     el::Helpers::installPostLogDispatchHandler(handler);
  
-    LOG(INFO) << "My log message";
+    LOG(INFO) << "This is my first message";
+    LOG(INFO) << "I am second in the line";
 
+    std::cout << std::endl; // Just enter an empty line to make it more readable
+
+    LOG(INFO) << "Following log messages have been dispatched so far! \n\t" << log_messages_dispatched;
     return 0;
 }
