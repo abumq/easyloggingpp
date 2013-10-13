7be0343da17024713935e88b34ecadf07d89948d
mkhan3189 <mkhan3189@gmail.com>
2013-10-13 12:45:48 +1100

    splitted installation and uninstallation for post log dispatch

--- test/post-log-dispatch-handler-test.h
+++ test/post-log-dispatch-handler-test.h
@@ -9,7 +9,7 @@ void postLogHandler(const LogMessage* msg) {
     loggedMessages.push_back(msg->message());
 }
 
-TEST(PostLogDispatchHandlerTest, EnsureHandlerIsCalled) {
+TEST(PostLogDispatchHandlerTest, Installation) {
     LOG(INFO) << "Log before handler installed";
     EXPECT_TRUE(loggedMessages.empty());
     
@@ -19,9 +19,14 @@ TEST(PostLogDispatchHandlerTest, EnsureHandlerIsCalled) {
     EXPECT_EQ(1, loggedMessages.size());
     std::string expectedMessage = "Should be part of loggedMessages - 1";
     EXPECT_EQ(expectedMessage, loggedMessages.at(0));
+}
+
+TEST(PostLogDispatchHandlerTest, Uninstallation) {
     
     // Uninstall handler
     Helpers::uninstallPostLogDispatchHandler();
+    LOG(INFO) << "This is not in list";
+    EXPECT_EQ(loggedMessages.end(), std::find(loggedMessages.begin(), loggedMessages.end(), "This is not in list"));
 }
 
 #endif // POST_LOG_DISPATCH_HANDLER_TEST_H
