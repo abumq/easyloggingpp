da7d21205cb3d904be3db366ac2389ee5b2a4e82
mkhan3189 <mkhan3189@gmail.com>
2013-10-19 18:48:04 +1100

    fixed configurator

--- samples/STL/configurator.cpp
+++ samples/STL/configurator.cpp
@@ -19,7 +19,6 @@ int main(void) {
     el::Loggers::reconfigureAllLoggers(confFromFile); 
      
     LOG(INFO) << "Info log after manually configuring 'default' logger";
-    LOG(FATAL) << "Fatal logging is off";
     el::Loggers::getLogger("default")->reconfigure();
     LOG(ERROR) << "Error log";
     LOG(WARNING) << "WARNING! log";
