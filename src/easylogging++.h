16db41023e9274723a6ff22426566f66df40938b
mkhan3189 <mkhan3189@gmail.com>
2013-10-26 15:30:10 +1100

    v9.35 release

--- src/easylogging++.h
+++ src/easylogging++.h
@@ -1,5 +1,5 @@
 //
-//  Easylogging++ v9.34 (development / unreleased version)
+//  Easylogging++ v9.35
 //  Single-header only, cross-platform logging library for C++ applications
 //
 //  Copyright (c) 2013 Majid Khan
@@ -4912,9 +4912,9 @@ public:
 class VersionInfo : base::StaticClass {
 public:
     /// @brief Current version number
-    static inline const std::string version(void) { return std::string("9.34"); }
+    static inline const std::string version(void) { return std::string("9.35"); }
     /// @brief Release date of current version
-    static inline const std::string releaseDate(void) { return std::string("23-10-2013 1844hrs"); }
+    static inline const std::string releaseDate(void) { return std::string("26-10-2013 1530hrs"); }
 };
 } // namespace el
 #undef VLOG_IS_ON
