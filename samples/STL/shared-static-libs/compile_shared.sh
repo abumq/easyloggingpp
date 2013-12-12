3b853490f66b7427542ba327c50ff3231efc2869
mkhan <mkhan@allhomes.com.au>
2013-12-13 10:30:38 +1100

    refactor for sample

--- samples/STL/shared-static-libs/compile_shared.sh
+++ samples/STL/shared-static-libs/compile_shared.sh
@@ -1,10 +1,12 @@
 rm -rf libmyLib.so lib/libmyLib.so lib/mylib.o lib/myLib.a myLib.a myapp logs ## Clean
 
 compiler=icpc ## Intel C++
+standard=c++11 ## If this does not work try c++0x (depends on your compiler)
+macros="-D_ELPP_THREAD_SAFE -D_ELPP_STACKTRACE_ON_CRASH"  ## Macros for library
 
 cd lib/
-$compiler --std=c++11 -pipe -fPIC -g -O0  -D_ELPP_THREAD_SAFE -D_ELPP_STACKTRACE_ON_CRASH  -Iinclude  -c -o mylib.o mylib.cpp
+$compiler --std=$standard -pipe -fPIC -g -O0  $macros  -Iinclude  -c -o mylib.o mylib.cpp
 $compiler -fPIC -g  -shared -o libmyLib.so mylib.o
 cp libmyLib.so ..
 cd ..
-$compiler -g -std=c++0x -fPIC -pipe  -Wl,-rpath=lib -L lib -lmyLib -Ilib/include  -o myapp myapp.cpp
+$compiler -g -std=$standard -fPIC -pipe  -Wl,-rpath=lib -L lib -lmyLib -Ilib/include  -o myapp myapp.cpp
--- samples/STL/shared-static-libs/compile_static.sh
+++ samples/STL/shared-static-libs/compile_static.sh
@@ -1,10 +1,12 @@
 rm -rf libmyLib.so lib/libmyLib.so lib/mylib.o lib/myLib.a myLib.a myapp logs ## Clean
 
 compiler=icpc ## Intel C++
+standard=c++11 ## If this does not work try c++0x (depends on your compiler)
+macros="-D_ELPP_THREAD_SAFE -D_ELPP_STACKTRACE_ON_CRASH"  ## Macros for library
 
 cd lib/
-$compiler --std=c++11 -pipe -fPIC -g -O0  -D_ELPP_THREAD_SAFE -D_ELPP_STACKTRACE_ON_CRASH  -Iinclude  -c -o mylib.o mylib.cpp
+$compiler --std=$standard -pipe -fPIC -g -O0 $macros -Iinclude  -c -o mylib.o mylib.cpp
 ar rvs myLib.a mylib.o
 cp myLib.a ..
 cd ..
-$compiler -g -std=c++11 -o myapp myapp.cpp -Ilib/include myLib.a
+$compiler -g -std=$standard -o myapp myapp.cpp -Ilib/include myLib.a
