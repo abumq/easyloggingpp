8355f97a728d1a2a34de5259f876ab2ee9fb433c
mkhan <mkhan@allhomes.com.au>
2014-04-09 09:08:01 +1000

    release 9.65

--- src/easylogging++.h
+++ src/easylogging++.h
@@ -1,5 +1,5 @@
 //
-//  Easylogging++ v9.64
+//  Easylogging++ v9.65
 //  Single-header only, cross-platform logging library for C++ applications
 //
 //  Copyright (c) 2014 Majid Khan
@@ -5465,9 +5465,9 @@ public:
 class VersionInfo : base::StaticClass {
 public:
    /// @brief Current version number
-    static inline const std::string version(void) { return std::string("9.64"); }
+    static inline const std::string version(void) { return std::string("9.65"); }
    /// @brief Release date of current version
-    static inline const std::string releaseDate(void) { return std::string("06-04-2014 1652hrs"); }
+    static inline const std::string releaseDate(void) { return std::string("09-04-2014 0907hrs"); }
 };
 }  // namespace el
 #undef VLOG_IS_ON
