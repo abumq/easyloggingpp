ad211392165ec0c21c6962320f155da27c7a1ee2
mkhan3189 <mkhan3189@gmail.com>
2014-04-13 14:40:08 +1000

    compile.sh upgrade

--- samples/STL/compile.sh
+++ samples/STL/compile.sh
@@ -2,12 +2,14 @@
 
 FILE=$1
 
+macro="$macro -D_ELPP_DEBUG_ERRORS"
 macro="$macro -D_ELPP_THREAD_SAFE"
 macro="$macro -D_ELPP_STL_LOGGING"
 macro="$macro -D_ELPP_LOG_UNORDERED_SET"
 macro="$macro -D_ELPP_LOG_UNORDERED_MAP"
 macro="$macro -D_ELPP_STACKTRACE_ON_CRASH"
 macro="$macro -D_ELPP_LOGGING_FLAGS_FROM_ARG"
+# macro="$macro -D_ELPP_DEFAULT_LOG_FILE=\"/a/path/that/does/not/exist/f.log\""
 
 if [ "$2" = "" ];then
   COMPILER=g++
