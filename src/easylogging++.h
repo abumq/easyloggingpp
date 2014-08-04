df57335117119e376d0eb1a307384b397650b2be
mkhan <mkhan@allhomes.com.au>
2014-08-05 09:38:22 +1000

    turned source to dev version

--- src/easylogging++.h
+++ src/easylogging++.h
@@ -1,5 +1,5 @@
 //
-//  Easylogging++ v9.74
+//  Easylogging++ v9.74 (development / unreleased version)
 //  Single-header only, cross-platform logging library for C++ applications
 //
 //  Copyright (c) 2014 Majid Khan
