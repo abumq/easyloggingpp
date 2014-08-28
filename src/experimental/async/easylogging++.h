b6a0ce921e2abb139b10670955b5b0d9b9aa7cea
Majid Khan <support@easylogging.org>
2014-08-28 21:17:54 +1000

    moved experimental

--- src/experimental/async/easylogging++.h
+++ src/experimental/async/easylogging++.h
@@ -1,5 +1,5 @@
 // Header for sample that sub-includes original header from src/ folder
 #ifndef EASYLOGGING_FOR_SAMPLES_H
 #define EASYLOGGING_FOR_SAMPLES_H
-#include "../../src/easylogging++.h"
+#include "../../easylogging++.h"
 #endif // EASYLOGGING_FOR_SAMPLES_H
