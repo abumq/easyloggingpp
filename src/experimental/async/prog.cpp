003e54504f30eb0b657b28ae47123e399bd7ec66
mkhan <mkhan@allhomes.com.au>
2014-08-29 09:53:22 +1000

    extreme testing

--- src/experimental/async/prog.cpp
+++ src/experimental/async/prog.cpp
@@ -15,12 +15,15 @@ int main(int argc, char *argv[])
     // _INIT_SYSLOG("my_proc", LOG_PID | LOG_CONS | LOG_PERROR, LOG_USER);
     Loggers::reconfigureAllLoggers(ConfigurationType::ToStandardOutput, "false");
 
-    int MAX_LOOP = 10000;
+    std::cout << "Starting program" << std::endl;
+    int MAX_LOOP = 1000000;
     for (int i = 0; i <= MAX_LOOP; ++i) {
         LOG(INFO) << "Log message " << i;
     }
     int result = MyMath::sum(1, 2);
     result = MyMath::sum(1, 3);
+
+    std::cout << "Finished program - now cleaning..." << std::endl;
 
     // SYSLOG(INFO) << "This is syslog - read it from /var/log/syslog";
 
