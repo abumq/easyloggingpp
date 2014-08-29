7de689c9502910b7378d849cc30a075a6354cf0a
mkhan <mkhan@allhomes.com.au>
2014-08-29 11:26:38 +1000

    updated as merged

--- src/experimental/async/prog.cpp
+++ src/experimental/async/prog.cpp
@@ -1,20 +1,16 @@
 
-//#include "async-logging.h"
 #include "easylogging++.h"
 #include "mymath.h"
 
 _INITIALIZE_EASYLOGGINGPP
-//AsyncLogQueue logQueue;
-//AsyncDispatchWorker asyncDispatchWorker;
 
-TIMED_SCOPE(benchmark, "benchmark");
+TIMED_SCOPE(benchmark, "benchmark-program");
 
 int main(int argc, char *argv[])
 {
-    //AsyncLogDispatchCallback::removeDefaultAndInstall();
     // _INIT_SYSLOG("my_proc", LOG_PID | LOG_CONS | LOG_PERROR, LOG_USER);
     el::Loggers::reconfigureAllLoggers(el::ConfigurationType::ToStandardOutput, "false");
-    TIMED_BLOCK(loggingPerformance, "loggingPerformance") {
+    TIMED_BLOCK(loggingPerformance, "benchmark-block") {
         el::base::MillisecondsWidth mwidth(3);
         std::cout << "Starting program " << el::base::utils::DateTime::getDateTime("%h:%m:%s", &mwidth) << std::endl;
         int MAX_LOOP = 1000000;
