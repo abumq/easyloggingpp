e946b7101a60226368a199afcd1fec796b043d58
mkhan <mkhan@allhomes.com.au>
2015-01-07 10:22:57 +1100

    restored develop branch

--- src/easylogging++.h
+++ src/easylogging++.h
@@ -1,5 +1,5 @@
 //
-//  Easylogging++ v9.79
+//  Easylogging++ v9.79 (development / unreleased version)
 //  Single-header only, cross-platform logging library for C++ applications
 //
 //  Copyright (c) 2014 Majid Khan
