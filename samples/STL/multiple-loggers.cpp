5545d30e396038d26fcc8f840fa8d47ad2c6a8a2
mkhan <mkhan@allhomes.com.au>
2015-01-06 14:59:17 +1100

    update sample

--- samples/STL/multiple-loggers.cpp
+++ samples/STL/multiple-loggers.cpp
@@ -6,9 +6,6 @@
  // @author mkhan3189
  //
 
-// You can also uncomment this line and achieve same result using LOG(..) macro
-// #define _LOGGER "default", "network"
-
 #include "easylogging++.h"
 
 _INITIALIZE_EASYLOGGINGPP
@@ -20,6 +17,15 @@ int main(void) {
     el::Loggers::getLogger("network"); // Register 'network' logger
     
     CLOG(INFO, "default", "network") << "My first log message that writes with network and default loggers";
+
+
+    // Another way of doing this may be
+    #define _LOGGER "default", "network"
+    CLOG(INFO, _LOGGER) << "This is done by _LOGGER";
+
+    // More practical way of doing this
+    #define NETWORK_LOG(LEVEL) CLOG(LEVEL, _LOGGER)
+    NETWORK_LOG(INFO) << "This is achieved by NETWORK_LOG macro";
 
     return 0;
 }
