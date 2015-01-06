4864e719457bfa2892cab57f71ebcc0560753a6e
mkhan <mkhan@allhomes.com.au>
2015-01-06 12:44:57 +1100

    moved experimental/async to samples

--- samples/async/easylogging++.h
+++ samples/async/easylogging++.h
@@ -1,5 +1,5 @@
 // Header for sample that sub-includes original header from src/ folder
 #ifndef EASYLOGGING_FOR_SAMPLES_H
 #define EASYLOGGING_FOR_SAMPLES_H
-#include "../../easylogging++.h"
+#include "../../src/easylogging++.h"
 #endif // EASYLOGGING_FOR_SAMPLES_H
