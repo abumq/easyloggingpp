c5c7d29b2bf28e7cffd54b97c1f17b36cf67a3db
mkhan <mkhan@allhomes.com.au>
2015-11-03 15:25:23 +1100

    Removed commented code

--- samples/STL/logrotate-pthread.cpp
+++ samples/STL/logrotate-pthread.cpp
@@ -6,19 +6,19 @@
 //
 // Compile: g++ -std=c++11 -Wall -Werror logrotate.cpp -lpthread -o logrotate -DELPP_THREAD_SAFE
 //
-// Revision 1.0
+// Revision 1.1
 // @author mkhan3189
 //
 
 #define ELPP_NO_DEFAULT_LOG_FILE
 #include "easylogging++.h"
-#include <unistd.h>
+#include <unistd.h> // for sleep()
 
 INITIALIZE_EASYLOGGINGPP
 
 void* logRotate(void*){
 	// Rotate every 20 seconds
-	while(true){
+	while (true){
 		sleep(20);
 		LOG(INFO) << "About to rotate log file!";
 		el::Loggers::getLogger("default")->reconfigure();
@@ -26,13 +26,12 @@ void* logRotate(void*){
 	return NULL;
 }
 
-int main(int,char**){
+int main(int, char**){
     el::Loggers::configureFromGlobal("logrotate.conf");
-    LOG(INFO)<<"The program has started!";
+    LOG(INFO) << "The program has started!";
     
     pthread_t logRotatorThread;
-	pthread_create(&logRotatorThread, NULL, logRotate, (void*)"0");
-    //pthread_join(logRotatorThread, NULL);
+	pthread_create(&logRotatorThread, NULL, logRotate, NULL);
     
     //Main thread
     for(int n = 0; n < 60; ++n){
