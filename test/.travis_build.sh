61dbe94432b6546a33ee9239dd915f476896b299
Majid Khan <mkhan3189@gmail.com>
2016-12-31 16:26:50 +1100

    troubleshoot travis CI

--- test/.travis_build.sh
+++ test/.travis_build.sh
@@ -1,5 +1,5 @@
 echo "Compiling unit tests..."
-clang++ *.cc -lgtest -I /usr/lib/ -std=c++11 -lpthread -o unit_test -D_ELPP_DEFAULT_LOG_FILE='"logs/el.gtest.log"' -DELPP_FEATURE_ALL -Wall -Wextra -pedantic -pedantic-errors -Werror -Wfatal-errors -Wundef
+clang++ *.cc -lgtest -std=c++11 -lpthread -o unit_test -D_ELPP_DEFAULT_LOG_FILE='"logs/el.gtest.log"' -DELPP_FEATURE_ALL -Wall -Wextra -pedantic -pedantic-errors -Werror -Wfatal-errors -Wundef -v
 echo "Running unit tests..."
 ./unit_test -v
 result=$?
