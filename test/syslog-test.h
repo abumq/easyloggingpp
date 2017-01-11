36d01047e09020cf9f4256ea5be45aa78813181a
mkhan <mkhan@allhomes.com.au>
2017-01-11 16:21:28 +1100

    Fixed test and release notes

--- test/syslog-test.h
+++ test/syslog-test.h
@@ -6,6 +6,7 @@
 #include "test.h"
 
 static const char* kSysLogFile = "/var/log/syslog";
+static const char* s_currentHost = el::base::utils::OS::currentHost().c_str();
 
 TEST(SysLogTest, WriteLog) {
     if (!fileExists(kSysLogFile)) {
